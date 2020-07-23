58575bb7bd6eae95377effe7cfdf6a3136919eaf
Jose Trinidad <internick10@yahoo.com.mx>
2020-07-23 10:04:51 -0500

    add local services

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  info: InfoPagina = {};
+  cargado = false;
+
+  constructor(private http: HttpClient) { 
+
+    console.log('info pagina cargada...');
+
+    this.http.get('assets/data/data-pagina.json').subscribe( (resp: InfoPagina) => {
+      this.cargado = true;
+      this.info = resp;
+      console.log(resp);
+    });
+
+  }
+}
