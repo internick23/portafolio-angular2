149c76f075df1f200f00a4a43635822eb1ba40e6
Jose Trinidad <internick10@yahoo.com.mx>
2020-07-29 10:11:54 -0500

    add search

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -10,16 +10,63 @@ export class ProductosService {
   url:string = 'https://angular-html2-8287d.firebaseio.com/productos_idx.json'
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor(private http: HttpClient) {
     this.cargarProductos();
    }
 
    private cargarProductos(){
-     this.http.get(this.url).subscribe((resp:Producto[]) => {
-       console.log(resp);
-       this.productos = resp;
-       this.cargando = false;
-     });
+    return new Promise((resolve, reject) => {
+      this.http.get(this.url).subscribe((resp:Producto[]) => {
+        this.productos = resp;
+        this.cargando = false;
+        resolve();
+      });
+    });
    }
+
+   getProducto(id: string){
+    return this.http.get(`https://angular-html2-8287d.firebaseio.com/productos/${id}.json`);
+   }
+
+
+   buscarProducto(termino: string){
+    if(this.productos.length === 0) {
+      this.cargarProductos().then(() => {
+         this.filtrarProductos(termino); 
+      });
+    } else {
+         this.filtrarProductos(termino);
+    }
+  }
+
+
+  private filtrarProductos(termino: string){
+    console.log(this.productos);
+    this.productosFiltrado = [];
+
+    termino = termino.toLowerCase();
+
+    this.productos.forEach(prod => {
+      const tituloLower = prod.titulo.toLowerCase();
+      if(prod.categoria.indexOf(termino) >= 0 || tituloLower.indexOf(termino) >= 0 ){
+        this.productosFiltrado.push(prod);
+      }
+    });
+  }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
